edb3c61da732fbfb3912a4da2e3a44c16fc07372
rkorom <richard.korom@gmail.com>
2023-10-09 16:32:01 +0200

    Updated default image

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import cv2
 from PIL import ImageTk, Image
 import os
 
-image_name = "image.png"
+image_name = "camera.png" if os.path.exists("camera.png") else "assets/thumbnail.jpg"
 
 
 def create_picture():
     camera = cv2.VideoCapture(0)
     result, image = camera.read()
     if result:
-        cv2.imwrite(image_name, image)
+        cv2.imwrite("camera.png", image)
         change_img()
 
 
 def change_img():
-    img2 = ImageTk.PhotoImage(Image.open(image_name))
+    img2 = ImageTk.PhotoImage(Image.open("camera.png"))
     label.configure(image=img2)
     label.image = img2
 
@@ -27,7 +27,7 @@ root.geometry("600x600")
 btn = Button(root, text="Create image", bd="5", command=create_picture)
 btn.pack(side="top")
 
-frame = Frame(root, width=600, height=400)
+frame = Frame(root, width=300, height=300)
 frame.pack()
 frame.place(anchor="center", relx=0.5, rely=0.5)
 
